fe37d710200c32935e4dc17d9d107e190da6dedf
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 01:25:39 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,7 @@
+var personName = "laiqa khan";
+
+console.log(personName.toLowerCase());
+
+console.log(personName.toLowerCase());
+
+console.log(personName.replace(/\b\w/g,(char) => char.toUpperCase()));
